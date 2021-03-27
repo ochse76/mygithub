cfd6d93237ae8f50edb3e9042d2a8ce94916fbe0
ochse76 <ochse@naver.com>
2021-03-27 20:09:11 +0900

    test

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val rollButton: Button = findViewById(R.id.button3)
         rollButton.setOnClickListener { OnDiceButtonClick() }
     }
